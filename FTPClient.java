e73f836a7a53fcfa8f1c3f310e2252e7fa143ad3
Ayush Khare <ayushkhare111@gmail.com>
2020-07-27 01:59:57 +0530

    Text file transfer successful

--- FTPClient.java
+++ FTPClient.java
@@ -3,13 +3,14 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
+import java.io.FileOutputStream;
 
 public class FTPClient {
     public static void main(String[] args) throws IOException {
-      if (args.length < 2) {
-                  System.err.println("Syntax: FTPClient <hostName or IPV4 address> <port>");
-                  return;
-              }
+        if (args.length < 2) {
+            System.err.println("Syntax: FTPClient <hostName or IPV4 address> <port>");
+            return;
+        }
         System.out.println("$Client: Client activated...\n");
 
         // Get a datagram socket
@@ -19,7 +20,7 @@ public class FTPClient {
         DatagramPacket packetClient;
 
         // Get an ip address of localhost or the server
-        InetAddress ip= InetAddress.getByName(args[0]);
+        InetAddress ip = InetAddress.getByName(args[0]);
 
         // Get port number
         int port = Integer.valueOf(args[1]);
@@ -37,7 +38,7 @@ public class FTPClient {
             bufClient = str.getBytes();
 
             // Sending request to server
-            packetClient = new DatagramPacket(bufClient,bufClient.length,ip,port);
+            packetClient = new DatagramPacket(bufClient, bufClient.length, ip, port);
             socketClient.send(packetClient);
 
             // Checking if client wants to stop
@@ -49,11 +50,21 @@ public class FTPClient {
 
             // Recieving response (ECHO) from server
             byte[] recieveData = new byte[1024];
-            DatagramPacket echoRecieved = new DatagramPacket(recieveData,recieveData.length);
+            DatagramPacket echoRecieved = new DatagramPacket(recieveData, recieveData.length);
             socketClient.receive(echoRecieved);
 
-            String echoprint=new String(echoRecieved.getData());
-            System.out.println( echoprint.trim());
+            // Converting recieved byte data into String 
+            String echoPrint = new String(echoRecieved.getData()).trim();
+            System.out.println("$Client: Server\'s response - " + echoPrint);
+
+            // Creating a new file and writing the recieved text file data to it
+            FileOutputStream fileOut = new FileOutputStream("./abc.txt", true);
+            char[] ch = echoPrint.toCharArray();
+            for (int i = 0; i < ch.length; i++) {
+                fileOut.write(ch[i]);
+            }
+            fileOut.close();
+
 
             System.out.println();
         }
--- FTPServer.java
+++ FTPServer.java
@@ -3,13 +3,14 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.io.File;
+import java.io.FileInputStream;
 
 public class FTPServer {
     public static void main(String[] args) throws IOException {
-      if (args.length < 1) {
-                  System.out.println("Syntax: FTPServer <port>");
-                  return;
-              }
+        if (args.length < 1) {
+            System.out.println("Syntax: FTPServer <port>");
+            return;
+        }
 
         System.out.println("$Server: Server started...\n");
 
@@ -21,7 +22,7 @@ public class FTPServer {
 
         // Creating byte array to recieve and send message in bytes
         byte[] bufServer = new byte[1024];
-        byte[] senddata = new byte[1024];
+        byte[] sendData = new byte[1024];
 
         while(true) {
             // Receiving a request
@@ -31,30 +32,34 @@ public class FTPServer {
 
             String requestString = new String(packetServer.getData());
             System.out.println(requestString.trim());
+
             StringBuilder sb = new StringBuilder("\n");
-            if(requestString.trim().equals("ls"))
-            {
 
-                    String dirName = "/";
+            if(requestString.trim().equals("file")) {
 
-                    File fileName = new File(dirName);
-                    File[] fileList = fileName.listFiles();
+                // Reading text file 
+                FileInputStream fileIn = new FileInputStream("./hello.txt");
+                int i;
+                do{
+                    i = fileIn.read();
+                    if (i != -1) {
+                        sb.append((char)i);
+                    }
+                } while (i != -1);
+                fileIn.close();
 
-                    for (File file: fileList)
-                          sb.append(file+"\n");
-                    //System.out.println(file);
-        }
-        else
-        {
-          sb.append("Unknown command\nWait for next update");
-        }
+            } else {
+                // If Client types command other than 'file'
+                sb.append("Unknown command, correct command - \'file\'\n$Client: Waiting for next update...");
+            }
 
-            //System.out.println(requestString.trim());
-
+                
             InetAddress IPAddress = packetServer.getAddress();
             int port = packetServer.getPort();
-            //echoData = requestString.getBytes();
-            senddata = (sb.toString()).getBytes();
+
+            // Converting data to bytes
+            sendData = (sb.toString()).getBytes();
+
             // Checking if Client wants to stop
             if (requestString.trim().equals("stop")) {
                 System.out.println("$Server: Client wants to stop");
@@ -62,14 +67,15 @@ public class FTPServer {
             }
 
             // Send the response to Client (ECHO)
-            DatagramPacket echoPacket = new DatagramPacket(senddata, senddata.length, IPAddress, port);
+            DatagramPacket echoPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
             socketServer.send(echoPacket);
-            System.out.println("response sent ");
+            System.out.println("$Server: Response sent.");
+            
             // Clear buffer after every message;
             bufServer = new byte[1024];
 
             System.out.println();
-        }
+            }
 
         // Closing the server
         System.out.println("$Server: Server shuting down...");
